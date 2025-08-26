9ca8f4433d8544d2f88e6c98540c96426a5cf118
Algath <n3177713@gmail.com>
2025-08-26 10:31:12 +0200

    correction gitignore, test.yml et widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,13 +2,22 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:gtk_flutter/main.dart';
+import 'package:provider/provider.dart';
+
+import '../lib/main.dart'; // ou le bon chemin vers ton MyApp
+import '../lib/app_state.dart'; // ou le bon chemin
 
 void main() {
-  testWidgets('Basic rendering', (tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const App());
+  testWidgets('Basic rendering', (WidgetTester tester) async {
+    // On wrappe MyApp avec le provider
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (_) => ApplicationState(),
+        child: App(),
+      ),
+    );
 
     // Verify that our counter starts at 0.
     expect(find.text('Firebase Meetup'), findsOneWidget);
